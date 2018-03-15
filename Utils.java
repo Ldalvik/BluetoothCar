2fc1f472b96235b7525d2760c0dec240aea8d53a
Julian Osborne <subaruforge@gmail.com>
2018-03-15 13:51:45 -0400

    Create Utils.java

---
+++ Utils.java
@@ -0,0 +1,12 @@
+package root.bluetooth;
+
+/**
+ * Created by student on 2/14/2018.
+ */
+
+public class Utils {
+
+    public static String command(int left, int right){
+        return left+","+right+"/";
+    }
+}
